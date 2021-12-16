a4889ff525992e653b1d45424ee13f1b2eaa8965
Dominiek <dominiekbroux@hotmail.com>
2021-12-16 15:42:26 +0100

    Gatsby plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 // Stap 1: Importeer React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 // Stap 2: definieer je component
 const IndexPage = () => {
   return (
     <main>
-      <title>Home Page</title>
       <Layout pageTitle = "Welcome bij Omni Vrembo">
       <p>Lorem ipsum</p>
+      <StaticImage
+        alt="Omni Vrembo"
+        src="../images/omnivrembo.jpg"
+      />
       </Layout>
     </main>
   )
