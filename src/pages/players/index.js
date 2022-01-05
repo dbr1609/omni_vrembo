96a122b0e0621cb9737831ee64733650d5322672
Dominiek <dominiekbroux@hotmail.com>
2022-01-05 16:32:28 +0100

    players page - opmaak en styling

--- src/pages/players/index.js
+++ src/pages/players/index.js
@@ -1,37 +1,89 @@
 import * as React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../../components/layout'
-const PlayersPage = ({data: {allWpPlayer: {edges}}}) => {
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import Player from "../../components/player"
+import {
+    playersPicture,
+    section,
+    subtitle,
+    players,
+    description,
+} from "../../page.module.css"
+
+const PlayersPage = ({
+    data: {
+        allWpPlayer: {edges: playersInfo},
+        wpPage: {playersPage},
+    },
+}) => {
+    const image = getImage(playersPage.headerPlayers.picture.localFile)
   return (
     <Layout pageTitle="Spelers van Omni Vrembo">
-      {edges.map((item) => {
-            const player = item.node.playerMeta;
-            const slug = item.node.slug;
-            return <Link to={`/players/${slug}`}>
-                <p key={item.node.id}>{player.personal.familyname} {player.personal.firstname} {player.capabilities.ranking}</p>
-            </Link>
-      })}
+        <GatsbyImage
+            className={playersPicture}
+            image={image}
+            alt={playersPage.headerPlayers.picture.altText}
+        />
+        <div className={section}>
+            <h2 className={subtitle}> {playersPage.headerPlayers.title}</h2>
+            <div
+                className={description}
+                dangerouslySetInnerHTML={{
+                    __html: playersPage.headerPlayers.description,
+                }}
+            />
+            <div className={players}>
+                {playersInfo.map(({ node: player}) => (
+                    <Player key={player.id} slug={player.slug} player={player} />
+                ))}
+            </div>
+        </div>
     </Layout>
   )
 }
 
 export const query = graphql`
     query {
+        wpPage(slug: {eq: "players"}) {
+            playersPage {
+              headerPlayers {
+                description
+                title
+                picture {
+                  localFile {
+                    childImageSharp {
+                      gatsbyImageData(quality: 100, placeholder: BLURRED, layout: FULL_WIDTH)
+                    }
+                  }
+                  altText
+                }
+              }
+            }
+        }
         allWpPlayer {
             edges {
-                node {
-                    id
-                    slug
-                    playerMeta {
-                        personal {
-                            familyname
-                            firstname
-                        }
-                        capabilities {
-                            ranking
-                        }
+              node {
+                playerMeta {
+                  personal {
+                    firstname
+                    familyname
+                  }
+                  capabilities {
+                    ranking
+                  }
+                  profilePicture {
+                    localFile {
+                      childImageSharp {
+                        gatsbyImageData(placeholder: BLURRED, transformOptions: {grayscale: true})
+                      }
                     }
+                    altText
+                  }
                 }
+                id
+                slug
+              }
             }
         }
     }
